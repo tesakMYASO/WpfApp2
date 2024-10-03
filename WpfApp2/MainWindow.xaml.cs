2c7cd16bd4a29e68682f3349c3cc0a173293421b
исп-34 <-34@LAB30-06>
2024-10-03 11:17:13 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using ClassLibrary1;
+
+namespace RandomNumberMaxFinder
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(CountTextBox.Text, out int n) && n > 0)
+            {
+                Random random = new Random();
+                int[] randomNumbers = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    randomNumbers[i] = random.Next(0, n + 1);
+                }
+
+                NumbersTextBlock.Text = string.Join(", ", randomNumbers);
+                MaxTextBlock.Text = randomNumbers.Max().ToString();
+            }
+            else
+            {
+                MessageBox.Show(" Введите положительное целое число для n.");
+            }
+        }
+
+      
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Автор: Исаев Арсений" + "\n" + " Группа ИСП-34 " + "\n" + " Задание: Найти максимум из n целых случайных чисел X, распределенных в диапазоне от 0 до n. Вывести на экран на одной строке сгенерированные числа, на другой строке результат ");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
